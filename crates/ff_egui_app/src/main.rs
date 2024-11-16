f68712873ad797420847629d353a10701427ada3
dbalsom <dbalsom@gmail.com>
2024-11-16 18:29:38 -0500

    deploy workflow to publish to github pages

--- crates/ff_egui_app/src/main.rs
+++ crates/ff_egui_app/src/main.rs
@@ -28,11 +28,10 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use ff_egui_app::APP_NAME;
-
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
+    use ff_egui_app::APP_NAME;
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
     let native_options = eframe::NativeOptions {
