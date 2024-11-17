ef52f13e2dda17fcb067bfc077676d5a69d07ef5
dbalsom <dbalsom@gmail.com>
2024-11-16 22:26:29 -0500

    fix script names

--- crates/ff_egui_app/assets/worker.js
+++ crates/ff_egui_app/assets/worker.js
@@ -1,11 +1,11 @@
-import wasm_bindgen, { closure_worker_entry_point } from "./ffweb.js";
+import wasm_bindgen, {closure_worker_entry_point} from "./ff_egui_app.js";
 
 self.onmessage = async event => {
 
     console.log("In worker.js with event.data[0] = " + event.data[0] + " and event.data[1] = " + event.data[1]);
 
     await wasm_bindgen({
-        path: "./ffweb_bg.wasm",
+        path: "./ff_egui_app_bg.wasm",
         memory: event.data[0],
     });
 
