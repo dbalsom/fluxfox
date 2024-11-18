97e733ebe473d679eae4503b6c05e910dc791414
dbalsom <dbalsom@gmail.com>
2024-11-17 21:39:32 -0500

    move min width under vertical ui?

--- crates/ff_egui_lib/src/widgets/track_list.rs
+++ crates/ff_egui_lib/src/widgets/track_list.rs
@@ -66,8 +66,8 @@ impl TrackListWidget {
                 ui.vertical(|ui| {
                     for (ti, track) in self.track_list.iter().enumerate() {
                         ui.group(|ui| {
-                            ui.set_min_width(150.0);
                             ui.vertical(|ui| {
+                                ui.set_min_width(400.0);
                                 ui.heading(format!("Track {}", track.ch));
                                 egui::Grid::new(format!("track_list_grid_{}", ti))
                                     .striped(true)
