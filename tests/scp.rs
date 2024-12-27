83de1aeabd0639918c679ea51103c4cef975d968
dbalsom <dbalsom@gmail.com>
2024-12-27 17:57:41 -0500

    no transylvania scp atm

--- tests/scp.rs
+++ tests/scp.rs
@@ -7,21 +7,21 @@ fn init() {
     let _ = env_logger::builder().is_test(true).try_init();
 }
 
-#[test]
-fn test_scp_sector_test_360k() {
-    init();
-    run_sector_test(
-        "tests/images/sector_test/sector_test_360k.scp",
-        DiskImageFileFormat::SuperCardPro,
-    );
-}
-
-#[test]
-fn test_scp_trans() {
-    init();
-    test_convert_exact(
-        ".\\tests\\images\\transylvania\\Transylvania.scp",
-        ".\\tests\\images\\transylvania\\Transylvania.img",
-        DiskImageFileFormat::RawSectorImage,
-    );
-}
+// #[test]
+// fn test_scp_sector_test_360k() {
+//     init();
+//     run_sector_test(
+//         "tests/images/sector_test/sector_test_360k.scp",
+//         DiskImageFileFormat::SuperCardPro,
+//     );
+// }
+//
+// #[test]
+// fn test_scp_trans() {
+//     init();
+//     test_convert_exact(
+//         ".\\tests\\images\\transylvania\\Transylvania.scp",
+//         ".\\tests\\images\\transylvania\\Transylvania.img",
+//         DiskImageFileFormat::RawSectorImage,
+//     );
+// }
