4a99fa3b08d499970ec7fd6b338f0d7caa240ea7
dbalsom <dbalsom@gmail.com>
2025-05-11 16:05:10 -0400

    remove unused binding

--- crates/ff_egui_app/assets/load_worker.js
+++
@@ -1,8 +0,0 @@
-import wasm_bindgen, {load_worker_entry_point} from "./ff_egui_app.js";
-
-self.onmessage = async event => {
-    await wasm_bindgen({
-        path: "./ff_egui_app_bg.wasm",
-    });
-    load_worker_entry_point(event.data)
-}
