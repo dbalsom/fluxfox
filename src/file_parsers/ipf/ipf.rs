7dc3f3e9517f5a18bd4243ae5e521efe360b8b8e
dbalsom <dbalsom@gmail.com>
2025-05-11 15:55:47 -0400

    fix chunk data size Option

--- src/file_parsers/ipf/ipf.rs
+++ src/file_parsers/ipf/ipf.rs
@@ -99,8 +99,8 @@ impl IpfParser {
         _ = image.seek(std::io::SeekFrom::Start(0));
 
         // The first chunk in an IPF file must be the CAPS chunk
-        // Pass a data limit of 0 so we don't end up reading a huge chunk from an invalid file.
-        if let Ok(file_header) = IpfChunk::read_args(&mut image, (0,)) {
+        // Pass a data limit of Some(0) so we don't end up reading a huge chunk from an invalid file.
+        if let Ok(file_header) = IpfChunk::read_args(&mut image, (Some(0),)) {
             if file_header.chunk_type == Some(IpfChunkType::Caps) {
                 detected = true;
             }
